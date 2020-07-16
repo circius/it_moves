3f94857ecca99e02f55d06fa87f78bbdc30888c3
circius <circius@github>
2020-07-16 22:27:03 +0100

    formatting fixes, bugfixes, a mixed bag.

---
+++ gm_phase_descriptions.lua
@@ -0,0 +1,71 @@
+gm_phase_descriptions = {
+  Setup={
+    title="Setup",
+    summary=[[
+    i. setup "missions remaining until rescue" track.
+    ii. build a deck of size (4 x # of players), with 1 joker.
+    iii. from that deck, deal 4 cards to each player.
+    iv. generate and place mission cards until their total difficulty > (2 * #players)
+    v. narrate the current situation.
+    vi. delete this card - a more useful one is below.
+    ]]
+  },
+  Briefing={
+    title="Briefing",
+    summary=[[
+    i. blindfold everyone (resolution button)
+    ii. Deal a Joker to the current thing, if they don't already have one.
+    iii. deal to players until everyone holds 4 cards.
+    iv. take off blindfolds.
+    v. add mission cards until the difficulty is high enough.
+    vi. narrate the new situation.
+    ]]
+  },
+  Assignments={
+    title="Assignments",
+    summary=[[
+    i. players discuss, in general terms, what they intend to do.
+    ii. at some point, intervene to stop the discussion.
+    iii. instruct the players to place their tokens on the missions they will work on.
+    ]]
+  },
+  Work={
+    title="Work",
+    summary=[[
+    i. each player commits 2-3 cards to the task they put their token on.
+    i.a at this point the Thing may play their Joker as part of their work.
+    ]]
+  },
+Debrief={
+    title="Debrief",
+    summary=[[
+    for each task,
+    i. gather all the cards played on it into a deck.
+    ii. put the deck onto the card-counter index card.
+    iii. decide if the status of the task is "No Progress", "Partially Complete" or "Complete".
+    iv. return the deck to the task-card and narrate the new situation - DO NOT disclose success-numbers.
+    v. if a task is complete, place it in the Mission Bag, and put its action cards on the discard table.
+    ]]
+  },
+  Isolation={
+    title="Isolation",
+    summary=[[
+    instruct the players that:
+    i. if any player wants to isolate another player, they can call for a vote.
+    ii. if a second layer supports the first, then players will vote on the matter.
+     - if there is a vote, each player places 0-1 cards near the completion tracker.
+     - if the number of successes played is >= (#players/2) then the isolation takes place.
+    iii. if someone was isolated, remind the players of the isolation rules.
+    ]]
+  },
+  Assimilation={
+    title="Assimilation",
+    summary=[[
+    i. blindfold all the players (reslution button)
+    ii. then say, "If you assimilated someone in the current round, take your
+      blindfold off and indicate them with your mouse." ALWAYS DO THIS.
+    iii. if the Thing did play a joker, and does specify someone, that person is assimilated.
+    iv. next turn, that person will receive a joker.
+    ]]
+  }
+}
