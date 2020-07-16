e6d4be35a0187c16a8fb70e9890886a86ea2e356
circius <circius@github>
2020-07-16 11:03:25 +0100

    added random mission generator and dummy data

---
+++ random_missions.lua
@@ -0,0 +1,77 @@
+missions={
+{
+name = [[Repair equipment]],
+difficulty = 5,
+state_0 = nil
+},
+{
+name = [[Fix lighting]],
+difficulty = 4,
+state_0 = nil
+},
+{
+name = [[Mysterious noises]],
+difficulty = 6,
+state_0 = nil
+},
+{
+name = [[Search for item]],
+difficulty = 5,
+state_0 = nil
+},
+{
+name = [[Rescue sled dogs]],
+difficulty = 3,
+state_0 = nil
+},
+{
+name = [[Call for help]],
+difficulty = 6,
+state_0 = nil
+},
+{
+name = [[Collect resources]],
+difficulty = 3,
+state_0 = nil
+},
+{
+name = [[Boost morale]],
+difficulty = 3,
+state_0 = nil
+},
+{
+name = [[Barricade]],
+difficulty = 6,
+state_0 = nil
+},
+{
+name = [[Examine corpse]],
+difficulty = 8,
+state_0 = nil
+},
+{
+name = [[Repair machinery]],
+difficulty = 5,
+state_0 = nil
+},
+{
+name = [[Prepare meals]],
+difficulty = 4,
+state_0 = nil
+},
+{
+name = [[Follow tracks]],
+difficulty = 4,
+state_0 = nil
+},
+{
+name = [[Repair heating]],
+difficulty = 5,
+state_0 = nil
+},
+{
+name = [[Prepare expeditionb]],
+difficulty = 9,
+state_0 = nil
+}
+}
