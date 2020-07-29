8c468902fa5e53c299b48098aded1b5b3aa0715c
circius <circius@posteo.de>
2020-07-29 18:06:42 +0100

    updated random missions

--- random_missions.lua
+++ random_missions.lua
@@ -1,77 +1,102 @@
 missions={
 {
-name = [[Repair equipment]],
+name = [[Repair equipment :: 5]],
 difficulty = 5,
-state_0 = nil
+state_0 = [[Strange, this had its regular maintenance check only last week. No way we can do without it though, so better get the toolbox out...]]
 },
 {
-name = [[Fix lighting]],
+name = [[Fix lighting :: 4]],
 difficulty = 4,
-state_0 = nil
+state_0 = [[Another blown bulb. It's dangerous enough inside when its dark, but you don't want to be out in the blizzard without guide lights!]]
 },
 {
-name = [[Mysterious noises]],
+name = [[Mysterious noises :: 6]],
 difficulty = 6,
-state_0 = nil
+state_0 = [[What the hell is that!? Sounds like the hounds of hell. Someone's got to go and investigate...]]
 },
 {
-name = [[Search for item]],
+name = [[Search for item :: 5]],
 difficulty = 5,
-state_0 = nil
+state_0 = [[Why is it that you keep tripping over something when you don't need it, but the moment you do... it's nowhere to be found!]]
 },
 {
-name = [[Rescue sled dogs]],
+name = [[Rescue sled dogs :: 3]],
 difficulty = 3,
-state_0 = nil
+state_0 = [[They've been trapped in the cold without any food for days now. Better get them somewhere safer.]]
 },
 {
-name = [[Call for help]],
+name = [[Call for help :: 6]],
 difficulty = 6,
-state_0 = nil
+state_0 = [[The storm is disrupting transmissions but it might just be possible to get something out.]]
 },
 {
-name = [[Collect resources]],
+name = [[Collect resources :: 3]],
 difficulty = 3,
-state_0 = nil
+state_0 = [[Someone's got to do the grunt work around here, even if it means going to every building in the camp!]]
 },
 {
-name = [[Boost morale]],
+name = [[Boost morale :: 3]],
 difficulty = 3,
-state_0 = nil
+state_0 = [[It's not looking good, but I can't let the others think that. Time for a party!]]
 },
 {
-name = [[Barricade]],
+name = [[Barricade :: 6]],
 difficulty = 6,
-state_0 = nil
+state_0 = [[Whatever is out there cannot be allowed to get in here. There must be something heavy enough to block the door here...]]
 },
 {
-name = [[Examine corpse]],
+name = [[Examine corpse :: 8]],
 difficulty = 8,
-state_0 = nil
+state_0 = [[This corpse is barely recognisable as human. Where could it have come from? Is it one of the scientists? But they're all here... aren't they?]]
 },
 {
-name = [[Repair machinery]],
+name = [[Repair machinery :: 5]],
 difficulty = 5,
-state_0 = nil
+state_0 = [[Twelve different size wrenches and the only one missing is the one that tightens this bolt. Typical!]]
 },
 {
-name = [[Prepare meals]],
+name = [[Prepare meals :: 4]],
 difficulty = 4,
-state_0 = nil
+state_0 = [[Safer if the cook is watched while they prepare the meals. Not that there's much to cook in here.]]
 },
 {
-name = [[Follow tracks]],
+name = [[Follow tracks :: 4]],
 difficulty = 4,
-state_0 = nil
+state_0 = [[Footprints going off into the snow. No-one has gone out though, have they?]]
 },
 {
-name = [[Repair heating]],
+name = [[Repair heating :: 5]],
 difficulty = 5,
-state_0 = nil
+state_0 = [[Without the radiators everything will be frozen solid in a matter of hours]]
 },
 {
-name = [[Prepare expeditionb]],
-difficulty = 9,
-state_0 = nil
+name = [[Prepare expedition :: 5]],
+difficulty = 5,
+state_0 = [[If we can't get rescued, we'll have to make it out on our own. That means gathering supplies.]]
+},
+{
+name = [[Science experiment :: 5]],
+difficulty = 5,
+state_0 = [[Perhaps we can learn something about this thing by studying the samples from the original dig...]]
+},
+{
+name = [[First aid :: 5]],
+difficulty = 5,
+state_0 = [[More than one person is injured now. We should put some time into treating these injuries properly, before they get worse.]]
+},
+{
+name = [[Refuel helicopter :: 7]],
+difficulty = 7,
+state_0 = [[The chopper isn't going anywhere without full tanks - and some to spare in the cabin. Got to get the pump working, the fuel line fitted, the jerry cans filled... Not an easy job!]]
+},
+{
+name = [[Screen the others :: 6]],
+difficulty = 6,
+state_0 = [[A physical examination of everyone here is probably wise at this point. A bit of a logistical nightmare though, since some are not exactly amenable.]]
+},
+{
+name = [[Prepare weapons :: 5]],
+difficulty = 5,
+state_0 = [[Best to be on the safe side. There must be something in here that can be fashioned into a weapon!]]
 }
 }
